c9e45bcc7fa593f6864d10f2559a8d9e5d8285bf
Alvin Oktavianus <agentsam1995@gmail.com>
2022-08-26 13:46:26 +0700

    Add prop types

--- src/components/SampleComponent.js
+++ src/components/SampleComponent.js
@@ -1,5 +1,11 @@
+import PropTypes from 'prop-types'
+
 const SampleComponent = props => {
-  return <div>{props.children}</div>
+  return <div id={props.htmlId}>{props.children}</div>
+}
+
+SampleComponent.propTypes = {
+  htmlId: PropTypes.string.isRequired,
 }
 
 export default SampleComponent
